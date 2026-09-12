79ad040def955e4ac539170a4a8358b2fb6e2b5c
Abhishek <abhishkumar159@gmail.com>
2026-09-12 12:52:05 +0530

    Add dictionary and set operations examples

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+marks = {
+    "harry" : 88,
+    "abhishek" : 100
+}
+
+print(marks, type(marks)) 
+
+
+
+
+s1 = {1, 2, 3, 4, 5}
+s2 = {5, 6, 7, 8}
+print(type(s1))
+
+s1 = s1.intersection({2, 7, 8, 9})
+print(s1)
