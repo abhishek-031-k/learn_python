e1fff513fc9f56c212e4244ed6e40a2ffd94bf33
Abhishek <abhishkumar159@gmail.com>
2026-09-13 19:06:16 +0530

    Add loop examples with for-else construct and simplified list iteration

---
+++ loops1.py
@@ -0,0 +1,19 @@
+# # # i = 0
+# # # while(i < 8):
+# # #     print(i)
+# # #     i += 1             
+
+# # l = [1, "abhishek", False, "print"]
+# # i = 0
+# # while i < len(l):
+# #     print(l[i])
+# #     i += 1 
+
+# l = [1, 2, 3, 4, 5, 6, 7, 8]
+# for i in l:
+#     print(i)
+l = [1, 2, 3]
+for item in l:
+    print(item)
+else:
+    print("done")    
